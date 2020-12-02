c8f8c3598e88eed68838f585820140e584e006b7
siddhigupta7668 <69845861+siddhigupta7668@users.noreply.github.com>
2020-12-02 11:36:27 +0530

    Your Favourites

---
+++ index.js
@@ -0,0 +1,161 @@
+var name = prompt("What's Your Name ?", "");
+
+if (name == null || name == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + name+ "! How are you today?";
+}
+
+
+var Fast = prompt("What's Your Favourite Fast Food ?", "");
+
+if (Fast  == null || Fast  == "") 
+{  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + Fast  + "! How are you today?";
+}
+
+console.log (name + "'s Favourite Fast Food Is " + Fast )
+
+var Healthy  = prompt("What's Your Favourite Healthy Food ?", "");
+
+if (Healthy   == null || Healthy  == "")
+ {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + Healthy  + "! How are you today?";
+}
+
+console.log (name + "'s Favourite Healthy Food Is " + Healthy)
+/*
+var None = prompt("What's Your Favourite Fast Food", "");
+
+if (None == null || None == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + None+ "! How are you today?";
+}
+*/
+
+var drink = prompt("What's Your Favourite Drink ?", "");
+
+if (drink == null || drink == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + drink+ "! How are you today?";
+}
+
+console.log (name + "'s Favourite Drink Is " + drink)
+
+var colour = prompt("What's Your Favourite Colour ?", "");
+
+if (colour == null || colour == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + colour+ "! How are you today?";
+}
+
+console.log (name + "'s Favourite Colour Is " + colour)
+
+var placeooc = prompt("What's Your Favourite Place In Foriegn ?", "");
+
+if (placeooc == null || placeooc == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + placeooc+ "! How are you today?";
+}
+
+console.log (name + "'s Favourite Place In Foriegn Is " + placeooc)
+
+var place = prompt("What's Your Favourite Place In India ?", "");
+
+if (place == null || place == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + place+ "! How are you today?";
+}
+
+console.log (name +"'s favourite Place In India Is " + place)
+
+var subject = prompt("What's Your Favourite Subject", "");
+
+if (subject == null || subject == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + subject+ "! How are you today?";
+}
+
+console.log(name + "'s Favourite Subject is " + subject)
+
+var gameindoor = prompt("What's Your Favourite Indoor Game ?", "");
+
+if (gameindoor == null || gameindoor == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + gameindoor+ "! How are you today?";
+}
+
+console.log (name +"'s Favourite Indoor Game Is " +gameindoor)   
+
+var gameoutdoor = prompt("What's Your Favourite Outdoor Gmae ?", "");
+
+if (gameoutdoor == null || gameoutdoor == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + gameoutdoor+ "! How are you today?";
+}
+
+console.log (name + "'s Favourite Outdoor Game Is "  +gameoutdoor)
+
+var car = prompt("What's Your Favourite Car", "");
+
+if (car == null || car == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + car+ "! How are you today?";
+}
+
+console.log (name +"'s Favourite Car Is " + car)
+
+
+var icecream = prompt("What's Your Favourite Icecream Flavour ", "");
+
+if (icecream == null || icecream == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + icecream+ "! How are you today?";
+}
+
+console.log(name +"'s Favourite Icecream Flavour Is " + icecream)
+
+
+
+
+var female = prompt("Who's Your Favourite Female Celebrity", "");
+
+if (female == null || female == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + female+ "! How are you today?";
+}
+
+console.log (name +"'s Favourite Female Celebrity Is " + female)
+
+var male = prompt("What's Your Favourite Male Celebrity", "");
+
+if (male == null || male == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + male+ "! How are you today?";
+}
+
+console.log (name +"'s Favourite Male Celebrity Is " + male )
+var superhero = prompt("Who's Your Favourite Superhero", "");
+
+if (superhero == null || superhero == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + superhero+ "! How are you today?";
+}
+
+console.log (name + "'s Favourite Superhero Is " + superhero)
